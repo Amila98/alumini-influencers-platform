65dbe87cce0f14777a36b85c38f30b50d0b8d552
Amila Gamanayake <84111918+Amila98@users.noreply.github.com>
2026-05-15 21:20:46 +0530

    Log DB_USER and check DB_PASSWORD existence

--- alumini-influencers-api/server.js
+++ alumini-influencers-api/server.js
@@ -13,6 +13,8 @@ console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_NAME:', process.env.DB_NAME);
 console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD EXISTS:', !!process.env.DB_PASSWORD);
 
 const express = require("express");
 const cors = require("cors");
